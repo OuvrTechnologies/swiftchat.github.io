6e8c804a4315eff6bb96834edb8be429868f711e
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2024-01-23 10:07:44 +0300

    Update server.js

--- versionDiscordBotAI/server.js
+++ versionDiscordBotAI/server.js
@@ -1,12 +1,21 @@
+// Import the express module
 const express = require('express');
+
+// Initialize the express application
 const server = express();
- 
+
+// Define the response for all requests to the root path
 server.all('/', (req, res) => {
-  res.send(`Bot online, now please copy the link above and paste it in uptimerobot to make this bot 24/7 working!`)
-})
- 
+  res.send(`Bot online, now please copy the link above and paste it in uptimerobot to make this bot 24/7 working!`);
+});
+
+// Define the keepAlive function
 function keepAlive() {
-  server.listen(3000, () => { console.log("Server is Ready!!" + Date.now()) });
+  // Start the server on port 3000 and log a message to the console
+  server.listen(3000, () => {
+    console.log(`Server is Ready!! ${Date.now()}`);
+  });
 }
- 
+
+// Export the keepAlive function
 module.exports = keepAlive;
